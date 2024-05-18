a178647f4e1073261293a7bf2558a457c10e93fd
Félix Antonio Pacheco Lobos <pachecolobos.felix@gmail.com>
2024-05-18 13:27:47 -0400

    Add Se generan archivos estáticos para deploy de Django

--- Backend/my_site/settings/production.py
+++ Backend/my_site/settings/production.py
@@ -1,5 +1,4 @@
 import os
-
 from my_site.settings.common import *
 
 # Quick-start development settings - unsuitable for production
@@ -8,12 +7,16 @@ from my_site.settings.common import *
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
+print(SECRET_KEY)
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 # SECURITY WARNING: update this when you have the production host
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1',
+                 's15-20-n-python-react.onrender.com']
 
+CSRF_TRUSTED_ORIGINS = ['https://s15-20-n-python-react.onrender.com']
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
@@ -21,10 +24,10 @@ ALLOWED_HOSTS = []
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.psycopg',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('POSTGRES_HOST'),
+        'PORT': os.environ.get('POSTGRES_PORT'),
     }
 }
