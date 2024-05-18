d617f384e069490a6265b87be118aaa02333e4d3
Félix Antonio Pacheco Lobos <pachecolobos.felix@gmail.com>
2024-05-17 21:48:29 -0400

    Add Se instala Django-Rest-Framework, se inicia proyecto React con Javascript con VITE, se inicia proyecto con Django

---
+++ Backend/my_site/settings/production.py
@@ -0,0 +1,30 @@
+import os
+
+from my_site.settings.common import *
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+# SECURITY WARNING: update this when you have the production host
+ALLOWED_HOSTS = []
+
+
+# Database
+# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.psycopg',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+# WorkFlow Lab
+
+WorkFlow lab es una aplicación dedicada a la gestión y administración de proyectos y su flujo de trabajo, esta pensada para ser una alternativa a otras plataformas con un enfoque dirigido al desarrollo ágil de proyectos donde cada etapa del flujo de trabajo importa, ya sea el diseño UI/UX como el código.
+
 Proyecto para No-Country creado por equipo Selección 15 - 20 - Noche - Python - React
