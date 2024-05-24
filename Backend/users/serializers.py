3c4382a83e0accb63bb9e17148cae90cfe6622db
Félix Antonio Pacheco Lobos <pachecolobos.felix@gmail.com>
2024-05-23 21:52:07 -0400

    Fix Se arregla almacenaje de contraseña sin hashear, ahora se almacena de forma segura en la base y se consulta correctamente.

--- Backend/users/serializers.py
+++ Backend/users/serializers.py
@@ -1,10 +1,10 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from django.contrib.auth.password_validation import validate_password
 from django.core import exceptions
 
 # Instance of User model
-
+User = get_user_model()
 
 # Serializer for User model
 
@@ -14,3 +14,9 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['username', 'email', 'password', ]
         extra_kwargs = {'password': {'write_only': True}, }
+
+    def create(self, validated_data):
+        user = User(**validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
