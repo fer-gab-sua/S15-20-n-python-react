692fad79de5aa5242ef5cd3035312a632ec3edd3
Félix Antonio Pacheco Lobos <pachecolobos.felix@gmail.com>
2024-05-26 14:12:35 -0400

    Add Se añade información para documentación de API

--- Backend/teams/views.py
+++ Backend/teams/views.py
@@ -1,14 +1,33 @@
 from rest_framework import generics, permissions
 from teams.serializers import TeamSerializer, RoleTypeSerializer, CollaboratorSerializer
 from core.models import Team, RoleType, Collaborator
-
+from drf_spectacular.utils import extend_schema
 
 # Create your views here.
+
+
 class TeamList(generics.ListCreateAPIView):
     permission_classes = (permissions.AllowAny, )
     queryset = Team.objects.all()
     serializer_class = TeamSerializer
 
+    @extend_schema(
+        tags=['Equipos'],
+        request=TeamSerializer,
+        responses=TeamSerializer,
+        operation_id='Listado de Equipos',
+        description='Listado de todos los Equipos',
+    )
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    @extend_schema(
+        tags=['Equipos'],
+        request=TeamSerializer,
+        responses=TeamSerializer,
+        operation_id='Creación de Equipo',
+        description='Crea un nuevo Equipo',
+    )
     def post(self, request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
 
@@ -18,11 +37,71 @@ class TeamDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Team.objects.all()
     serializer_class = TeamSerializer
 
+    @extend_schema(
+        tags=['Equipos'],
+        request=TeamSerializer,
+        responses=TeamSerializer,
+        operation_id='Muestra un Equipo especificado',
+        description='Usado para mostrar el equipo especificado como parámetro en la URL.',
+    )
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    @extend_schema(
+        tags=['Equipos'],
+        request=TeamSerializer,
+        responses=TeamSerializer,
+        operation_id='Modificación del equipo especificado',
+        description='Usado para modificar un equipo especificado como parámetro en la URL, debe proporcionar todos los valores del equipo.',
+    )
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    @extend_schema(
+        tags=['Equipos'],
+        request=TeamSerializer,
+        responses=TeamSerializer,
+        operation_id='Modificación parcial del equipo especificado',
+        description='Modificación parcial del equipo especificado como parámetro en la URL. solo indique los valores que quiere modificar.',
+    )
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
+
+    @extend_schema(
+        tags=['Equipos'],
+        request=TeamSerializer,
+        responses=TeamSerializer,
+        operation_id='Eliminación del equipo especificado',
+        description='Elimina el equipo especificado como parámetro en la URL.',
+    )
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
 
 class RoleTypeList(generics.ListCreateAPIView):
     permission_classes = (permissions.AllowAny, )
     queryset = RoleType.objects.all()
     serializer_class = RoleTypeSerializer
+
+    @extend_schema(
+        tags=['Roles'],
+        request=RoleTypeSerializer,
+        responses=RoleTypeSerializer,
+        operation_id='Listado de Roles',
+        description='Listado de todos los Roles',
+    )
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    @extend_schema(
+        tags=['Roles'],
+        request=RoleTypeSerializer,
+        responses=RoleTypeSerializer,
+        operation_id='Creación de Roles',
+        description='Crea un nuevo Rol',
+    )
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
 
 class RoleTypeDetail(generics.RetrieveUpdateDestroyAPIView):
@@ -30,14 +109,114 @@ class RoleTypeDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = RoleType.objects.all()
     serializer_class = RoleTypeSerializer
 
+    @extend_schema(
+        tags=['Roles'],
+        request=RoleTypeSerializer,
+        responses=RoleTypeSerializer,
+        operation_id='Muestra un Rol especificado',
+        description='Usado para mostrar el equipo especificado como parámetro en la URL.',
+    )
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    @extend_schema(
+        tags=['Roles'],
+        request=RoleTypeSerializer,
+        responses=RoleTypeSerializer,
+        operation_id='Modificación del Rol especificado',
+        description='Usado para modificar un equipo especificado como parámetro en la URL, debe proporcionar todos los valores del equipo.',
+    )
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    @extend_schema(
+        tags=['Roles'],
+        request=RoleTypeSerializer,
+        responses=RoleTypeSerializer,
+        operation_id='Modificación parcial del Rol especificado',
+        description='Modificación parcial del equipo especificado como parámetro en la URL. solo indique los valores que quiere modificar.',
+    )
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
+
+    @extend_schema(
+        tags=['Roles'],
+        request=RoleTypeSerializer,
+        responses=RoleTypeSerializer,
+        operation_id='Eliminación del Rol especificado',
+        description='Elimina el equipo especificado como parámetro en la URL.',
+    )
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
 
 class CollaboratorList(generics.ListCreateAPIView):
     permission_classes = (permissions.AllowAny, )
     queryset = Collaborator.objects.all()
     serializer_class = CollaboratorSerializer
 
+    @extend_schema(
+        tags=['Colaboradores'],
+        request=CollaboratorSerializer,
+        responses=CollaboratorSerializer,
+        operation_id='Listado de Colaboradores',
+        description='Listado de todos los Equipos',
+    )
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    @extend_schema(
+        tags=['Colaboradores'],
+        request=CollaboratorSerializer,
+        responses=CollaboratorSerializer,
+        operation_id='Creación de Colaborador',
+        description='Crea un nuevo Equipo',
+    )
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
 
 class CollaboratorDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (permissions.AllowAny, )
     queryset = Collaborator.objects.all()
     serializer_class = CollaboratorSerializer
+
+    @extend_schema(
+        tags=['Colaboradores'],
+        request=CollaboratorSerializer,
+        responses=CollaboratorSerializer,
+        operation_id='Muestra un Colaborador especificado',
+        description='Usado para mostrar el equipo especificado como parámetro en la URL.',
+    )
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    @extend_schema(
+        tags=['Colaboradores'],
+        request=CollaboratorSerializer,
+        responses=CollaboratorSerializer,
+        operation_id='Modificación del Colaborador especificado',
+        description='Usado para modificar un equipo especificado como parámetro en la URL, debe proporcionar todos los valores del equipo.',
+    )
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    @extend_schema(
+        tags=['Colaboradores'],
+        request=CollaboratorSerializer,
+        responses=CollaboratorSerializer,
+        operation_id='Modificación parcial del Colaborador especificado',
+        description='Modificación parcial del equipo especificado como parámetro en la URL. solo indique los valores que quiere modificar.',
+    )
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
+
+    @extend_schema(
+        tags=['Colaboradores'],
+        request=CollaboratorSerializer,
+        responses=CollaboratorSerializer,
+        operation_id='Eliminación del Colaborador especificado',
+        description='Elimina el equipo especificado como parámetro en la URL.',
+    )
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
--- Backend/users/views.py
+++ Backend/users/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth import get_user_model
 from users.serializers import UserSerializer
 from rest_framework import generics, permissions
-
+from drf_spectacular.utils import extend_schema
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 # Instance of User model
 User = get_user_model()
@@ -10,12 +11,70 @@ User = get_user_model()
 
 
 class UserList(generics.ListCreateAPIView):
-    permission_classes = (permissions.AllowAny, )
     serializer_class = UserSerializer
     queryset = User.objects.all()
+
+    @extend_schema(
+        tags=['Usuarios'],
+        request=UserSerializer,
+        responses=UserSerializer,
+        operation_id='Listado de Usuarios',
+        description='Listado de todos los usuarios',
+    )
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    @extend_schema(
+        tags=['Usuarios'],
+        request=UserSerializer,
+        responses=UserSerializer,
+        operation_id='Creación de Usuario',
+        description='Adjunta un objeto con los valores indicados para crear un nuevo Usuario',
+    )
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
 
 class UserDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (permissions.AllowAny, )
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    @extend_schema(
+        tags=['Usuarios'],
+        request=UserSerializer,
+        responses=UserSerializer,
+        operation_id='Muestra un usuario especificado',
+        description='Usado para mostrar un usuario especificado como parámetro en la URL.',
+    )
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    @extend_schema(
+        tags=['Usuarios'],
+        request=UserSerializer,
+        responses=UserSerializer,
+        operation_id='Modificación del Usuario especificado.',
+        description='Adjunta un objeto con los valores indicados para modificar un usuario especificado como parámetro en la URL, debe proporcionar en el cuerpo de la petición todos los valores del objeto usuario.',
+    )
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    @extend_schema(
+        tags=['Usuarios'],
+        request=UserSerializer,
+        responses=UserSerializer,
+        operation_id='Modificación de datos del Usuario especificado.',
+        description='Adjunta un objeto con los valores indicados para modificar un usuario especificado como parámetro en la URL, debe proporcionar en el cuerpo de la petición solo los datos a modificar.',
+    )
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
+
+    @extend_schema(
+        tags=['Usuarios'],
+        request=UserSerializer,
+        responses=UserSerializer,
+        operation_id='Borrado de Usuario especificado',
+        description='Elimina el registro del usuario especificado como parámetro en la URL.',
+    )
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
